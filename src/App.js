c0df34bd72f259c93a1b041e6d25c9b957e8805f
GYURIMDO <galaxy020422@gmail.com>
2024-09-08 21:20:28 -0600

    This is a working version of this app, first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis cumque ut fugit.</p>
+    </>
+  );
+}
+
+export default App;
